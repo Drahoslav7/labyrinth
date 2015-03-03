8bbd5fd16661cabdd0394abfb8b49f6f6b0c86ef
Drahoslav Bednář <drahoslav@yo2.cz>
2015-03-03 13:24:09 +0100

    nalinkovan boost

--- src/client.c
+++ src/client.c
@@ -1,5 +1,7 @@
+#include <boost/asio.hpp>
+
 int main(int argc, char const *argv[])
 {
-	
+
 	return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "boost/asio.hpp"
+#include <boost/asio.hpp>
 #include "components.h"
 
 using namespace std;
