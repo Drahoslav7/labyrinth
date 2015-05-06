aadfda74ee79c860aace0829dc30a7772694a923
Jiří Kunčák <kuncak.jiri@gmail.com>
2015-05-06 17:52:28 +0200

    client parametry

--- src/client.cpp
+++ src/client.cpp
@@ -1,9 +1,29 @@
-// #include <boost/asio.hpp>
+#include <boost/asio.hpp>
 // #include <QtGui/QApplication>
 // #include <QApplication>
 
+using namespace std;
+
+struct address{
+	string hostname;
+	int port;
+};
+
 int main(int argc, char const *argv[])
 {
+	if(argc != 3){
+		cerr << "Usage: client <host> <port>\n" << endl;
+		return 1;
+	}else{
+		address serveraddrr;
+		serveraddr.hostname = argv[1];
+		serveraddr.port = argv[2];
+	}
+
 	
+
+	
+
+
 	return 0;
 }
