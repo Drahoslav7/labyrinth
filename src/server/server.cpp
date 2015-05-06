9e1dc0d1c740905bf3b232716f94de35518da58b
Drahoslav Bednář <drahoslav@yo2.cz>
2015-05-06 17:50:24 +0200

    refaktoring

---
+++ src/server/server.cpp
@@ -0,0 +1,23 @@
+#include "server.h"
+#include "../components.h"
+
+using namespace std;
+
+
+/**
+ * Vraci port zadany jako prvni argument programu
+ * nebo nulu pokud je neco spatne
+ * @param  argc 
+ * @param  argv 
+ * @return      port
+ */
+int Server::getPort(int argc, char const *argv[]){
+	int port = 0;
+	if(argc > 1)
+		port = atoi(argv[1]);
+	return port;
+}
+
+
+
+
