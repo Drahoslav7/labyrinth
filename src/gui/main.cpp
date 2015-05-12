f1db6e152d0854e92648d95932317ec5d8cecc43
Drahoslav Bednář <drahoslav@yo2.cz>
2015-05-12 21:26:24 +0200

    qt neco

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,32 +1,11 @@
-// #include <QApplication>
-// #include <QTextEdit>
-
-// int main(int argv, char **args)
-// {
-// 	QApplication app(argv, args);
-
-// 	QTextEdit textEdit;
-// 	textEdit.show();
-// 	return app.exec();
-// }
-
-#include <QtGui>
-#include <QApplication>
-#include <QTextEdit>
-#include <QPushButton>
-#include <QVBoxLayout>
+#include "labyrinth.h"
 
 int main(int argv, char **args)
 {
 	QApplication app(argv, args);
-	QTextEdit *textEdit = new QTextEdit;
-	QPushButton *quitButton = new QPushButton("&Quit");
-	QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
-	QVBoxLayout *layout = new QVBoxLayout;
-	layout->addWidget(textEdit);
-	layout->addWidget(quitButton);
-	QWidget window;
-	window.setLayout(layout);
-	window.show();
+	Labyrinth labyrinth;
+
+	labyrinth.show();
+
 	return app.exec();
 }
